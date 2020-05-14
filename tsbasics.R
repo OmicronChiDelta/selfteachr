2a53277377d493eb6b37e1c57445da83c8298cfa
Alex <50920163+OmicronChiDelta@users.noreply.github.com>
2020-05-14 11:09:26 +0100

    playing with tsibbles

--- tsbasics.R
+++ tsbasics.R
@@ -3,14 +3,45 @@ library(dplyr)
 library(tidyverse)
 library(fpp) #Book companion
 library(tsibbledata) #Toy datasets
+library(feasts) #so autoplot plays nicely
+library(lubridate)
 
-#Raw data
-my_data = read_csv('C:\\Users\\Alex White\\Documents\\GitHub\\reading_buses\\extractions\\16_20200101_20200401\\arrival_history.csv')
+### OLYMPIC RUNNING 
+#Basics of selection and filtering
+data = olympic_running
 
-test = my_data %>% select(LocationCode, ScheduledArrivalTime, arrival_delta)
+#Length is a factor...
+cat(str(data$Length))
 
-#Check for duplicates
-dups = duplicates(test, index='ScheduledArrivalTime')
+#So map it to a string to allow selection...
+data$Length = as.character(data$Length)
 
-#Convert to tsibble format
-my_tsibble = as_tsibble(test, index=ScheduledArrivalTime)
+#Time series of mens' 100m results 
+test = data%>%filter(Length == '100m')%>%filter(Sex == 'men')%>%select(Year, Time)
+
+#Show the time series
+test%>%autoplot(Time) +
+  xlab('Year') +
+  ylab('Time/s') 
+
+### AUSTRALIAN TOURISM
+#Totals for each quarter by state
+tots = tourism%>%
+  filter(Purpose == 'Business')%>%
+  group_by(State)%>%
+  summarise(Total = sum(Trips))
+
+#Raw time series
+tots%>%autoplot(Total) + xlab('Calendar Quarter') + ylab('Total trips')
+
+#Seasonal plot by quarter
+tots%>%gg_season(Total) + xlab('Quarter') + ylab('Total trips')
+
+
+
+### VICTORIA ELECTRICTY DEMAND
+v = vic_elec%>%mutate(CalYear = year(Date))
+
+#Scatter
+ggplot(v, aes(Temperature, Demand)) + geom_point(aes(color='red', alpha=0.001)) + xlab('Temperature/Celsius') + ylab('Electricity Demand') + ggtitle('Correlation between temperature and electricity demand')
+
